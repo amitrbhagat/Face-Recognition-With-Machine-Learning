21ba6555ace5fe5688af0bf64ea952fc1808baed
Amit <amitrbhagat6789@gmail.com>
2026-02-15 01:48:42 -0800

    upload the predicted images and report on the web app

--- 4_Flask_App/app/views.py
+++ 4_Flask_App/app/views.py
@@ -31,9 +31,31 @@ def genderapp():
         pred_image, predictions = faceRecognitionPipeline(path)
         pred_filename = 'prediction_image.jpg'
         cv2.imwrite(f'./static/predict/{pred_filename}', pred_image)
-        print("ML model predicted successfully") 
+        print("predictions") 
 
         #generate report
-        
+        report = []
+        for i, obj in enumerate(predictions):
+            gray_image = obj['roi'] # grayscale image (array) 
+            eigen_image = obj['eig_img'].reshape(100, 100) # eigen image (array)
+            gender_name = obj['prediction_name']# name
+            score = round(obj['score']*100,2) # Probability score
 
-    return render_template('gender.html') #GET Request  
+            #save grayscale and eigen in predict folder
+            gray_image_name = f'gray_{i}.jpg'
+            eigen_image_name = f'eigen_{i}.jpg'
+            matimg.imsave(f'./static/predict/{gray_image_name}', gray_image, cmap='gray')
+            matimg.imsave(f'./static/predict/{eigen_image_name}', eigen_image, cmap='gray')   
+
+            # save report
+            report.append([
+                gray_image_name,
+                eigen_image_name,
+                gender_name,
+                score
+            ])
+
+        return render_template('gender.html', fileupload = True, report=report) # POST Request
+
+
+    return render_template('gender.html', fileupload = False) # GET Request 
